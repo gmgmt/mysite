5f458552f981bd2bb010002000822b5c7e221ca3
pcw <3386993382@qq.com>
2019-11-19 21:16:12 +0800

    22

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mi9f+b3cb#k58-=4m4nv5e#sp(-0+!ie)pnvzrnm%l@(x3slwh'
+# SECRET_KEY = 'mi9f+b3cb#k58-=4m4nv5e#sp(-0+!ie)pnvzrnm%l@(x3slwh'
 
 
 
